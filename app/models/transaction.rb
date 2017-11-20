051b32aaab9f45900d48289f7495808f0d2964b0
Jared Guyer <jaredg8@gmail.com>
2017-11-20 18:39:41 +0100

    add devise to users

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -3,18 +3,6 @@ class Transaction < ApplicationRecord
   belongs_to :item
 
   validates :meetup_location, :status, :price, presence: true
-  # validates_uniqueness_of :user_id, :scope => [:user, item: [:user]]
-  # before_save :check_user_transaction
-
-
-
-
-
-  # def check_user_transaction
-  #   if self.user == self.item.user
-  #     errors.add("You cant book yourself.")
-  #   end
-  # end
 
 
 end
