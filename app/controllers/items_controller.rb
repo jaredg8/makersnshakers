8eba3a25726a6199613b38bc97ffa388e571c608
Jared Guyer <jaredg8@gmail.com>
2017-11-21 19:02:11 +0100

    added search bar to home page, this also includes updating index on item controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,45 +1,48 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:edit, :update, :destroy, :show]
 
-  def index
-    @items = Item.all
-  end
-  def show
-  end
+#within index add conditionals for search
+def index
+  # @items = Item.all
+  @items = Item.where(name: params[:search][:name])
+end
 
-  def new
-    @item = Item.new
-  end
+def show
+end
 
-  def create
-    @item = Item.new(item_params)
-    if @item.save
-      redirect_to @item
-    else
-      render 'new'
-    end
-  end
+def new
+  @item = Item.new
+end
 
-  def edit
-  end
-
-  def update
-    @item.update(item_params)
+def create
+  @item = Item.new(item_params)
+  if @item.save
     redirect_to @item
+  else
+    render 'new'
   end
+end
 
-  def destroy
-    @item.destroy
-    redirect_to root_path
-  end
+def edit
+end
 
-  private
+def update
+  @item.update(item_params)
+  redirect_to @item
+end
 
-  def set_item
-    @item = Item.find(params[:id])
-  end
+def destroy
+  @item.destroy
+  redirect_to root_path
+end
 
-  def item_params
+private
+
+def set_item
+  @item = Item.find(params[:id])
+end
+
+def item_params
   params.require(:item).permit(:name, :description, :price, :category, photos: [])
-  end
+end
 end
