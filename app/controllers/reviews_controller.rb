77cc371a30e8792ddf39b919d54bbd92e8271a7a
Jared Guyer <jaredg8@gmail.com>
2017-11-21 12:04:36 +0100

    updated controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,11 +1,40 @@
 class ReviewsController < ApplicationController
+  before_action :set_review, only: [:show, :destroy]
+
+  def show
+    @item = Item.find(params[:id])
+    @user = User.find(params[:id])
+
+  def new
+    @review = Review.new
+  end
+
   def create
     @review = Review.new(review_params)
     @review = Item.find(params[:item_id])
     @review = User.find(params[:user_id])
-
+    if review.save
+      redirect_to item_path(@item)
+    else
+      @items = Item.all?
+      render 'items/show'
+    end
   end
+
 
   def destroy
+    @item = @review.item
+    @review.destroy
+    redirect_to item_path(@item)
   end
+
+  private
+
+  def set_review
+    @review = Review.find(params[:id])
+  end
+
+  def review_params
+    params.require(:review).permit(:rating, :text, :user_id)
+
 end
