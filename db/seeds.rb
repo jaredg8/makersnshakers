22b170a5310adbfe64e1f2b787bc03d85b10b35f
Toni Panacek <toni.panacek@gmail.com>
2017-11-22 13:17:37 +0100

    changed seed file to include random berlin addresses

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,15 @@
 Item.destroy_all
 
-number = (1..30).to_a.sample
-street = %w(Friedrichstraße Warschauer Straße Prenzlauer Allee Oranienburger Straße Mehringdamm Leipziger Straße Sonnenallee Ackerstraße Ebertstraße Karl-Liebknecht-Straße Rosa-Luxemburg-Straße Frankfurter Allee).sample
-category = %w(Electronics Home & Garden Outdoors).sample
+number = (1..30).to_a
+street = %w(Friedrichstraße Warschauer Straße Prenzlauer Allee Oranienburger Straße Mehringdamm Leipziger Straße Sonnenallee Ackerstraße Ebertstraße Karl-Liebknecht-Straße Rosa-Luxemburg-Straße Frankfurter Allee)
+category = %w(Electronics Home & Garden Outdoors)
 
 20.times do
   item = Item.new(
     name: Faker::Commerce.product_name,
     price: Faker::Commerce.price,
-    category: "#{category}",
-    address: "#{number} #{street}, Berlin, Germany"
+    category: "#{category.sample}",
+    address: "#{number.sample} #{street.sample}, Berlin, Germany"
   )
   item.save!
   item.photo_urls = ['https://placeimg.com/640/480/tech', 'https://placeimg.com/640/480/tech']
