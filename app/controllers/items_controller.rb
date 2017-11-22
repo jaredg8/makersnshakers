5e73ab28584f02c6369ca514849fae1863fcb83b
Jared Guyer <jaredg8@gmail.com>
2017-11-22 11:08:49 +0100

    added search bar, it only works with the entire string of the name

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,48 +1,50 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:edit, :update, :destroy, :show]
 
-#within index add conditionals for search
-def index
-  # @items = Item.all
-  @items = Item.where(name: params[:search][:name])
-end
-
-def show
-end
-
-def new
-  @item = Item.new
-end
-
-def create
-  @item = Item.new(item_params)
-  if @item.save
-    redirect_to @item
-  else
-    render 'new'
+  def index
+    if params[:search]
+      @items = Item.where(name: params[:search][:name])
+    else
+      @items = Item.all
+    end
   end
-end
 
-def edit
-end
+  def show
+  end
 
-def update
-  @item.update(item_params)
-  redirect_to @item
-end
+  def new
+    @item = Item.new
+  end
 
-def destroy
-  @item.destroy
-  redirect_to root_path
-end
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to @item
+    else
+      render 'new'
+    end
+  end
 
-private
+  def edit
+  end
 
-def set_item
-  @item = Item.find(params[:id])
-end
+  def update
+    @item.update(item_params)
+    redirect_to @item
+  end
 
-def item_params
-  params.require(:item).permit(:name, :description, :price, :category, photos: [])
-end
+  def destroy
+    @item.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def item_params
+    params.require(:item).permit(:name, :description, :price, :category, photos: [])
+  end
 end
