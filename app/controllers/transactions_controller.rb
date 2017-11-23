91a383a8d3c07d220b446a87fcf5884f8fa2d867
Joao Martins <joaomarcelofm@gmail.com>
2017-11-23 12:15:30 +0100

    implemented date form / method to pass the booking details to transaction creation

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,11 +1,14 @@
 class TransactionsController < ApplicationController
 
+  before_action :set_item, only: [:new, :create]
+
   def new
-    @transction = Transaction.new
+    @transaction = Transaction.new
   end
 
   def create
-
+    @transaction = Transaction.new(transction_params)
+    @transaction.item = @item
   end
 
   def show
@@ -15,5 +18,9 @@ class TransactionsController < ApplicationController
   end
 
   def destroy
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 end
